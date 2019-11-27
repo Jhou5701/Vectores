a4db6b691b8fc279c610c7cbd26f6ee4f5896e94
JHOU JEROHAM RODRIGUEZ LAZ <jhou.rodriguez@pucese.edu.ec>
2019-11-27 13:00:04 -0500

    Terminado Arrays

--- Vectores/Form1.Designer.cs
+++ Vectores/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtingreso = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtsuma = new System.Windows.Forms.TextBox();
+            this.txtpromedio = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnagregar = new System.Windows.Forms.Button();
@@ -51,12 +51,12 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Ingresar un entero";
             // 
-            // textBox1
+            // txtingreso
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtingreso.Location = new System.Drawing.Point(165, 59);
+            this.txtingreso.Name = "txtingreso";
+            this.txtingreso.Size = new System.Drawing.Size(100, 20);
+            this.txtingreso.TabIndex = 1;
             // 
             // label2
             // 
@@ -68,25 +68,25 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Lista de enteros agregados";
             // 
-            // textBox2
+            // txtsuma
             // 
-            this.textBox2.Location = new System.Drawing.Point(364, 286);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtsuma.Location = new System.Drawing.Point(364, 255);
+            this.txtsuma.Name = "txtsuma";
+            this.txtsuma.Size = new System.Drawing.Size(120, 20);
+            this.txtsuma.TabIndex = 3;
             // 
-            // textBox3
+            // txtpromedio
             // 
-            this.textBox3.Location = new System.Drawing.Point(364, 326);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 20);
-            this.textBox3.TabIndex = 4;
+            this.txtpromedio.Location = new System.Drawing.Point(364, 295);
+            this.txtpromedio.Name = "txtpromedio";
+            this.txtpromedio.Size = new System.Drawing.Size(120, 20);
+            this.txtpromedio.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(290, 326);
+            this.label3.Location = new System.Drawing.Point(290, 295);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 19);
             this.label3.TabIndex = 5;
@@ -96,7 +96,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(290, 287);
+            this.label4.Location = new System.Drawing.Point(290, 256);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 19);
             this.label4.TabIndex = 6;
@@ -107,20 +107,22 @@
             this.btnagregar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnagregar.Location = new System.Drawing.Point(364, 153);
             this.btnagregar.Name = "btnagregar";
-            this.btnagregar.Size = new System.Drawing.Size(120, 23);
+            this.btnagregar.Size = new System.Drawing.Size(120, 28);
             this.btnagregar.TabIndex = 7;
             this.btnagregar.Text = "Agregar";
             this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
             // 
             // btnresultado
             // 
             this.btnresultado.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnresultado.Location = new System.Drawing.Point(364, 195);
             this.btnresultado.Name = "btnresultado";
-            this.btnresultado.Size = new System.Drawing.Size(120, 23);
+            this.btnresultado.Size = new System.Drawing.Size(120, 28);
             this.btnresultado.TabIndex = 8;
             this.btnresultado.Text = "Mostrar Resultado";
             this.btnresultado.UseVisualStyleBackColor = true;
+            this.btnresultado.Click += new System.EventHandler(this.btnresultado_Click);
             // 
             // Lista_Numeros
             // 
@@ -151,10 +153,10 @@
             this.Controls.Add(this.btnagregar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtpromedio);
+            this.Controls.Add(this.txtsuma);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtingreso);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -166,10 +168,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtingreso;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtsuma;
+        private System.Windows.Forms.TextBox txtpromedio;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnagregar;
--- Vectores/Form1.cs
+++ Vectores/Form1.cs
@@ -12,9 +12,53 @@ namespace Vectores
 {
     public partial class Form1 : Form
     {
+        private int cont = 0;
+        private const int MAX = 100;
+        private double[] vector = new double[MAX];
+        private void llenarlista()
+        { }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void btnagregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(cont<MAX)
+                {
+                    vector[cont] = int.Parse(this.txtingreso.Text);
+                }
+                cont++;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }            
+        }
+
+        private void btnresultado_Click(object sender, EventArgs e)
+        {
+            //AGREGAR ELEMENTOS AL LISTBOX
+            for(int i=0; i<cont;i++)
+            {
+                this.Lista_Numeros.Items.Add(vector[i]);
+            }
+            this.txtsuma.Text = sum().ToString();
+            this.txtpromedio.Text = division().ToString();
+        }
+        private double sum()
+        {
+            double suma = 0;
+            for (int i = 0; i < cont; i++)
+            {
+                suma = suma + vector[i];
+            }
+            return suma;
+        }
+        private double division()
+        {
+            return (sum() / cont);
         }
     }
 }
